535f766d25524386bc62b8cbab68f5c295722be4
unknown <ferielmaamer@yahoo.fr>
2023-06-26 22:20:36 -0400

    updated about section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,5 @@
 <template>
   <footer>
-    <h5>This task tracking application is designed to facilitate 
-      effective task organization, ensuring that no tasks are inadvertently overlooked or forgotten.</h5>
     <p>Copyright Feriel Maamer &copy; 2023</p>
     <router-link to="/about">About</router-link>
   </footer>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <h3>Version 1.0.0</h3>
+  <h5>This task tracking application is designed to facilitate 
+        effective task organization, ensuring that no tasks are inadvertently overlooked or forgotten.</h5>
   <router-link to="/">Go Back</router-link>
 </template>
 
